04063a1a7075099a093c73a23a1485b94b7e0e68
Bassnectar 2017_2 <bassnectar2017_2@MacBook-Pro.attlocal.net>
2020-06-06 10:09:38 -0700

    Updates

---
+++ precache-manifest.a95236d98fba38ceb65104587df2b3fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a22e1b6c20c5a1000c436ec51862ec1",
+    "url": "/volumeandco/index.html"
+  },
+  {
+    "revision": "6bfc05708d65e480eb04",
+    "url": "/volumeandco/static/css/main.1f5ae561.chunk.css"
+  },
+  {
+    "revision": "5934e42dbf12f5631e2a",
+    "url": "/volumeandco/static/js/2.07686186.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/volumeandco/static/js/2.07686186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bfc05708d65e480eb04",
+    "url": "/volumeandco/static/js/main.3e1064e4.chunk.js"
+  },
+  {
+    "revision": "aa398a8eec28ebfb51dd",
+    "url": "/volumeandco/static/js/runtime-main.bd64af36.js"
+  },
+  {
+    "revision": "34e254c6ed08c7cd1057d52a9b6599bb",
+    "url": "/volumeandco/static/media/VolCo_Logo_Mermaid_White.34e254c6.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04997c8e3f401e156f7f44562305ce41.js"
+  "/volumeandco/precache-manifest.a95236d98fba38ceb65104587df2b3fe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/volumeandco/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
